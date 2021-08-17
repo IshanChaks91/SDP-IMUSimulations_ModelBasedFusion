bc5b2d6a391aa1becb78c078045b64b58c391fae
Deji <deajay4life@gmail.com>
2021-08-18 00:26:22 +0200

    all tasks (user stories) and installation steps updated

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/deji/user_directory/install/bullet3/lib/pkgconfig/:/home/deji/user_directory/install/liblfds/lib/pkgconfig:/home/deji/user_directory/install/microblx/lib/pkgconfig
+export PATH=/home/deji/user_directory/install/microblx/bin:$PATH
+export LUA_PATH="/home/deji/user_directory/install/microblx/share/lua/5.1/?.lua;/home/deji/user_directory/install/uutils/usr/share/lua/5.1/?.lua"
+export UBX_PATH=/home/deji/user_directory/install/microblx
